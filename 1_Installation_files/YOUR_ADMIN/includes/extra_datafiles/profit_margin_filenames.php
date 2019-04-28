a9c31003e8342904b9d146e1a69f95858c3d79ae
Zen4All <info@zen4all.nl>
2019-04-28 22:44:56 +0200

    move define to new file
    Not using core files anymore

---
+++ 1_Installation_files/YOUR_ADMIN/includes/extra_datafiles/profit_margin_filenames.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * filenames.php
+ * Defines the filenames used in the project
+ *
+ * @package general
+ * @copyright Copyright 2003-2010 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version $Id: filenames.php 15519 2010-02-19 11:01:58Z drbyte $
+ * @private
+ */
+
+define('FILENAME_STATS_PRODUCTS_PROFIT', 'stats_products_profit');
