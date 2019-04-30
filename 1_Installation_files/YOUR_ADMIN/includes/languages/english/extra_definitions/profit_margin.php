2f807d9dcb4761b42daf6a47f7cc47dd3c46f437
Zen4All <info@zen4all.nl>
2019-04-30 14:50:17 +0200

    Update language files
    - consolidated multiple defines in to 1 file for easier maintenance

---
+++ 1_Installation_files/YOUR_ADMIN/includes/languages/english/extra_definitions/profit_margin.php
@@ -0,0 +1,12 @@
+<?php
+define('BOX_REPORTS_PRODUCTS_PROFIT', 'Products Profit');
+
+define('TABLE_HEADING_COST','Cost');
+define('TABLE_HEADING_NET_PROFIT','Margin %');
+
+define('TEXT_PRODUCTS_PRICE_MARGIN_CALCULATOR', '<b>Profit Margin Calculator</b>');
+define('TEXT_PRODUCTS_PRICE_COST', 'Products Cost:');
+define('TEXT_PRODUCTS_PRICE_MARKUP', 'Markup Percent :');
+define('TEXT_PRODUCTS_PRICE_GROSS', 'Products Price (Gross):');
+define('TEXT_PRODUCTS_PRICE_MARGIN_GROSS', 'Gross Margin $:');
+define('TEXT_PRODUCTS_PRICE_MARGIN_GROSS_PERCENT', 'Gross Margin %:');
