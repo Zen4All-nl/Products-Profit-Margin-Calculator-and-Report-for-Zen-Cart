81b830031c4485387192a81ea8b49445417d5c90
Zen4All <info@zen4all.nl>
2019-05-01 09:45:34 +0200

    add un-install

--- uninstall/product_margin_uninstall.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM admin_pages WHERE page_key='configProductFinder' LIMIT 1;
-DELETE FROM configuration WHERE configuration_key LIKE 'PRODUCT_FINDER_%';
-DELETE FROM configuration_group WHERE configuration_group_title = 'Product Finder Settings' LIMIT 1;
