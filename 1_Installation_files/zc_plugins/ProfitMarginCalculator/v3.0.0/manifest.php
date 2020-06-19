663845e0eabb34131d64796616d673c7e1d865de
Zen4All <info@zen4all.nl>
2020-06-19 09:05:29 +0200

    update manifest according to latest ZC guide

--- 1_Installation_files/zc_plugins/ProfitMarginCalculator/v3.0.0/manifest.php
+++ 1_Installation_files/zc_plugins/ProfitMarginCalculator/v3.0.0/manifest.php
@@ -1,15 +1,13 @@
 <?php
 
 return [
-
-    'pluginType' => 'free',
-    'pluginAuthor' => 'Zen4All (design75)',
+    'pluginVersion' => 'v3.0.0',
     'pluginName' => 'Products Profit/Margin Calculator + Report',
     'pluginDescription' => '<p>This contribution is a combination of the following:<ul><li>Profit Margin Calculator v1.12 by Farrukh</li><li>Products Profit and Cost Report v1 by Farrukh</li></ul></p><h5> What does this contribution do?</h5><p>It allows you to add a cost field to your products and allow you to track your profit. Its not a perfect contribution, but its on its way there. A report under Admin-_Reports will show you what your profit is</p>',
-    'pluginVersion' => 'v3.0.0',
+    'pluginAuthor' => 'Zen4All (design75)',
     'pluginId' => 175,
-    'managed' => true,
     'zcVersions' => ['v157'],
+    'changelog' => 'https://github.com/Zen4All-nl/Products-Profit-Margin-Calculator-and-Report-for-Zen-Cart/releases',
+    'github_repo' => 'https://github.com/Zen4All-nl/Products-Profit-Margin-Calculator-and-Report-for-Zen-Cart', // url
     'pluginGroups' => [],
-    'changelog' => [],
 ];
